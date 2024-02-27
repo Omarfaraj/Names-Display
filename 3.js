9bb6541052419a1ae43471c8f4332d0cbfd6d83e
blvck <omarfaraj355@gmail.com>
2024-02-27 12:45:55 +0300

    javascript-names

---
+++ 3.js
@@ -0,0 +1,15 @@
+var names = ['Faraj', 'Rashid', 'Zakariya', 'Halima', 'Abubakar'];
+
+function displayNames() {
+    var displayElement = document.GetElementById('nameDispaly');    
+
+    displayElement = innerHTML = '';
+
+    for (var i = 0; i < name.length; i++) {
+    paragraph.textContext = names[i];
+
+    displayElement.apendChild(paragraph);
+    }
+}
+
+displayNames();
